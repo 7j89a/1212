ea1c1a0e62944b6330a129e8b477e4fafe87e75c
7j89a <119797677+7j89a@users.noreply.github.com>
2024-12-10 17:59:19 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+#Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7920189349:AAEUbt2gpAqdNtgJxHcMHHz8GmL-zFxQ0x0")
+
+#Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "20944746"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "d169162c1bcf092a6773e685c62c3894")
+
+#Database 
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://yousufsalhab5:6KYS4zCC6a5MZLXJ@cluster0.z2lco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
